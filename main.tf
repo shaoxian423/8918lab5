786f743ddd705f50e9e0f42e739dc12acbe643ff
Shaoxian Duan <duan0027@algonquinlive.com>
2025-06-07 16:24:49 -0400

    Add public IP address definition

--- main.tf
+++ main.tf
@@ -47,3 +47,11 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.labelPrefix}-A05-RG"
   location = var.region
 }
+
+# Public IP Address
+resource "azurerm_public_ip" "main" {
+  name                = "${var.labelPrefix}-A05-PublicIP"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
