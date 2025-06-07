4303487ddf95f87c45d8d6dd9bd555a51c4a71d4
Shaoxian Duan <duan0027@algonquinlive.com>
2025-06-07 16:25:38 -0400

    Add virtual network definition

--- main.tf
+++ main.tf
@@ -55,3 +55,11 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
 }
+
+# Virtual Network
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.labelPrefix}-A05-VNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
