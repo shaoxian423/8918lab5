672fcb3fada07ab580e7ae3eedc6c44f663e9011
Shaoxian Duan <duan0027@algonquinlive.com>
2025-06-07 16:26:46 -0400

    Add subnet definition

--- main.tf
+++ main.tf
@@ -63,3 +63,11 @@ resource "azurerm_virtual_network" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
+
+# Subnet
+resource "azurerm_subnet" "main" {
+  name                 = "${var.labelPrefix}-A05-Subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
